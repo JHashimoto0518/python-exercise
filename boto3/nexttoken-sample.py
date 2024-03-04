3cbf27fcf25daa54037c72b566f2d3e6484ec624
Junichi Hashimoto <46337314+JHashimoto0518@users.noreply.github.com>
2024-03-05 08:00:48 +0900

    fix: credential error

--- boto3/nexttoken-sample.py
+++ boto3/nexttoken-sample.py
@@ -1,11 +1,22 @@
+import sys
 import boto3
 
-client = boto3.client("logs")
+# usage:
+# python3 nexttoken-sample.py <profile_name>
+#
+# output:
+# {'logGroups': [], 'ResponseMetadata': {'RequestId': 'ee66ee68-729e-4f15-804d-1b4c372e085f', 'HTTPStatusCode': 200, 'HTTPHeaders': {'x-amzn-requestid': 'ee66ee68-729e-4f15-804d-1b4c372e085f', 'content-type': 'application/x-amz-json-1.1', 'content-length': '16', 'date': 'Mon, 04 Mar 2024 22:59:05 GMT'}, 'RetryAttempts': 0}}
+# end first response
+# start next token loop
+
+profile_name = sys.argv[1]
+
+session = boto3.Session(region_name="ap-southeast-1", profile_name=profile_name)
+client = session.client("logs")
 response = client.describe_log_groups()
 
 print(response)
 print("end first response")
-print("Next token: " + response["nextToken"])
 
 print("start next token loop")
 while "nextToken" in response:
